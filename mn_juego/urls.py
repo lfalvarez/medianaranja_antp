9de2783d5b06780133f665076b9b2670248b2501
Felipe Álvarez <felipealvarez@letsta.lk>
2021-03-20 14:00:54 -0300

    Agregando rutas de distrito

--- mn_juego/urls.py
+++ mn_juego/urls.py
@@ -8,5 +8,5 @@ app_name = 'mn_juego'
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
     path('c/<slug:slug>', DistritoView.as_view(), name='distrito_detail'),
-    path('d/<slug:slug>', DistritoBySlug.as_view(), name='distrito_detail_by_distrito_slug'),
+    path('<slug:slug>', DistritoBySlug.as_view(), name='distrito_detail_by_distrito_slug'),
 ]
