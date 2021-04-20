4bf94d34a03d41f27fa855f4b86f56c7c17690b4
Felipe Álvarez <felipealvarez@letsta.lk>
2021-04-19 20:28:33 -0400

    Agregando vista de pueblos originarios

--- mn_juego/urls.py
+++ mn_juego/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 
-from mn_juego.views import IndexView, DistritoView, DistritoBySlug, ComunaView
+from mn_juego.views import (IndexView,
+                            DistritoView,
+                            DistritoBySlug,
+                            ComunaView,
+                            PuebloOriginarioDetailView)
 
 
 app_name = 'mn_juego'
@@ -10,4 +14,5 @@ urlpatterns = [
     path('c/<slug:slug>', DistritoView.as_view(), name='distrito_detail'),
     path('<slug:slug>', DistritoBySlug.as_view(), name='distrito_detail_by_distrito_slug'),
     path('comuna/<slug:slug>', ComunaView.as_view(), name='comuna_detail'),
+    path('pueblos_originarios/<slug:slug>', PuebloOriginarioDetailView.as_view(), name='pueblo_originario_detail'),
 ]
